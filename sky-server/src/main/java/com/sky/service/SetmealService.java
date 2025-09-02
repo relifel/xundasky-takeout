5fe6332ed0beb85643b4079c321d2b0be1f65f56
itcast <hello@itcast.cn>
2025-09-02 18:54:04 +0800

    商品浏览业务代码开发

--- sky-server/src/main/java/com/sky/service/SetmealService.java
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -2,7 +2,9 @@ package com.sky.service;
 
 import com.sky.dto.SetmealDTO;
 import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.entity.Setmeal;
 import com.sky.result.PageResult;
+import com.sky.vo.DishItemVO;
 import com.sky.vo.SetmealVO;
 
 import java.util.List;
@@ -27,4 +29,12 @@ public interface SetmealService {
     void update(SetmealDTO setmealDTO);
 
     void startOrStop(Integer status, Long id);
+    /**
+     * 条件查询
+     * @param setmeal
+     * @return
+     */
+    List<Setmeal> list(Setmeal setmeal);
+
+    List<DishItemVO> getDishItemById(Long id);
 }
