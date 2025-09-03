f6833cc310066bcf4cddfe54c70ddc3f467471d0
itcast <hello@itcast.cn>
2025-09-03 22:29:37 +0800

    添加购物车业务代码开发

---
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -0,0 +1,8 @@
+package com.sky.service;
+
+import com.sky.dto.ShoppingCartDTO;
+import org.springframework.stereotype.Service;
+
+public interface ShoppingCartService {
+    void add(ShoppingCartDTO shoppingCartDTO);
+}
