00b12a38be875ee645e4e066531c630bb7480e01
itcast <hello@itcast.cn>
2025-09-04 14:39:11 +0800

    查看购物车业务代码开发

--- sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -13,7 +13,7 @@ public interface ShoppingCartMapper {
     @Update("update shopping_cart set number = #{number} where id = #{id}")
     void updateNumberById(ShoppingCart shoppingCart);
 
-    @Insert("insert into shopping_cart (name, image, dish_id, setmeal_id, dish_flavor, number, amount, create_time) " +
-            "values (#{name}, #{image}, #{dishId}, #{setmealId}, #{dishFlavor}, #{number}, #{amount}, #{createTime})")
+    @Insert("insert into shopping_cart (name, user_id,image, dish_id, setmeal_id, dish_flavor, number, amount, create_time) " +
+            "values (#{name}, #{userId},#{image}, #{dishId}, #{setmealId}, #{dishFlavor}, #{number}, #{amount}, #{createTime})")
     void insert(ShoppingCart shoppingCart);
 }
--- sky-server/src/main/java/com/sky/service/ShoppingCartService.java
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -1,8 +1,13 @@
 package com.sky.service;
 
 import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 public interface ShoppingCartService {
     void add(ShoppingCartDTO shoppingCartDTO);
+
+    List<ShoppingCart> showShoppingCart();
 }
