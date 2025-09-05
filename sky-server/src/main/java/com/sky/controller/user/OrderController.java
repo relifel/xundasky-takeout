8078fc99923dfc67f4af1937c1bb6937bdca716e
itcast <hello@itcast.cn>
2025-09-05 20:21:46 +0800

    客户催单业务代码

--- sky-server/src/main/java/com/sky/controller/user/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -91,5 +91,10 @@ public class OrderController {
         orderService.repetition(id);
         return Result.success();
     }
+    @GetMapping("/reminder/{id}")
+    public Result reminder(@PathVariable("id") Long id){
+        orderService.reminder(id);
+        return Result.success();
+    }
 
 }
--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -80,4 +80,6 @@ public interface OrderService {
      * @param id
      */
     void complete(Long id);
+
+    void reminder(Long id);
 }
